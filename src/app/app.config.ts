2d95c763e040d9ad4d3fc0c734a956da2e3819c9
Math Peeters <pmathieu.peeters@gmail.com>
2026-05-20 18:05:20 +0200

    feat: create auth-interceptor.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,13 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import {provideHttpClient, withInterceptors, withInterceptorsFromDi} from '@angular/common/http';
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
+import {authInterceptor} from './shared/interceptors/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(withInterceptorsFromDi())
+    provideHttpClient(withInterceptors([authInterceptor]))
   ]
 };
---
+++ src/app/shared/interceptors/auth-interceptor.ts
@@ -0,0 +1,37 @@
+import { HttpInterceptorFn, HttpRequest, HttpHandlerFn, HttpEvent, HttpErrorResponse } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Observable, throwError, catchError } from 'rxjs';
+import { AuthService } from '../services/auth';
+import { Router } from '@angular/router';
+
+export const authInterceptor: HttpInterceptorFn = (
+  req: HttpRequest<unknown>,
+  next: HttpHandlerFn
+): Observable<HttpEvent<unknown>> => {
+
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  const token: string | null = authService.getAccessToken();
+
+  // Si on a un token, on l'ajoute au header
+  if (token) {
+    const authReq = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    return next(authReq).pipe(
+      catchError((error: HttpErrorResponse) => {
+        // Si 401 → session expirée → on déconnecte
+        if (error.status === 401) {
+          authService.logout();
+          router.navigate(['/login']);
+        }
+        return throwError(() => error);
+      })
+    );
+  }
+
+  return next(req);
+};
