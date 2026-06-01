71e9b7b37ce367c545fd2d72e287320b4a66a295
Math Peeters <pmathieu.peeters@gmail.com>
2026-06-01 15:18:13 +0200

    feat: create event-management (//todo).

---
+++ src/app/features/admin/events-management/events-management.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-events-management',
+  imports: [],
+  templateUrl: './events-management.html',
+  styleUrl: './events-management.scss',
+})
+export class EventsManagement {
+
+  //todo on en est ici
+
+}
